32b04522a4131e83c936c2c92674395ba9f4e1f4
Phanth0m <oettam.gnat@gmail.com>
2026-07-23 20:23:59 -0700

    Update template2.c

--- template2.c
+++ template2.c
@@ -1,16 +1,16 @@
-#include <kipr/wallaby.h>
+#include <kipr/wombat.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
 #include <string.h>
-
+#include <sys/select.h>
+#include <unistd.h>
 
 #define handlingLogic1() { \
     moves[length++] = (char)c; \
     break; \
 }
-
 /*
 	Created by Matteo Tang at a reasonable time of day. WIP. 7/20/2026
 	This is a simple program which reads user inputs or returns them
@@ -21,12 +21,9 @@
 	Use template 1 code to extend this if you would like.
 	THIS TEMPLATE CODE ASSUMES A 2 WHEELED BOT
 */
-
-const char toPlay[2048] = 'wsadx';//CHANGE WITH YOUR INPUTS
-
+const char toPlay[2048] = "wsadx";//CHANGE WITH YOUR INPUTS
 static int leftPort = 0;
 static int rightPort = 1;
-
 
 // Stops all motors instantly
 void resetMotors() {
@@ -46,8 +43,22 @@ void move(int power1, int power2, int time) {
 	resetMotors();
 }
 
+/*
+	Returns true if there is at least one character waiting on stdin, without blocking.
+	Uses select() with a zero timeout to poll the stdin file descriptor.
+*/
+int stdinHasInput(void) {
+	fd_set fds;
+	struct timeval tv = { 0, 0 };
+
+	FD_ZERO(&fds);
+	FD_SET(STDIN_FILENO, &fds);
+
+	return select(STDIN_FILENO + 1, &fds, NULL, NULL, &tv) > 0;
+}
+
 void playbackHandling(const char* toPlayback) {
-	
+
 	/*
 		This is where you would put things such as:
 		Reset
@@ -56,29 +67,27 @@ void playbackHandling(const char* toPlayback) {
 		Initialize potential constants
 		Auto shut off in
 	*/
-
-
 	for (int i = 0; i < strlen(toPlayback); i++) {
 		switch (toPlayback[i]) {
 		case 'W':
 		case 'w':
 			printf("moved forward");
-			//forward
+			move(70, 70, 300);
 			break;
 		case 'S':
 		case 's':
 			printf("moved back");
-			//back
+			move(-70, -70, 300);
 			break;
 		case 'A':
 		case 'a':
 			printf("moved left");
-			//left
+			move(-70, 70, 300);
 			break;
 		case 'D':
 		case 'd':
 			printf("moved right");
-			//right
+			move(70, -70, 300);
 			break;
 		case 'X':
 		case 'x':
@@ -98,7 +107,6 @@ char* writeHandling() {
 	if (moves == NULL) {
 		return NULL;
 	}
-
 	int c;
 	while ((c = getchar()) != EOF) {
 		if (length >= capacity - 1) {
@@ -110,23 +118,22 @@ char* writeHandling() {
 			}
 			moves = temp;
 		}
-
 		switch (c) {
 		case 'W':
 		case 'w':
-			//forward
+			move(70, 70, 300);
 			handlingLogic1();
 		case 'S':
 		case 's':
-			//backwards
+			move(-70, -70, 300);
 			handlingLogic1();
 		case 'A':
 		case 'a':
-			//left
+			move(-70, 70, 300);
 			handlingLogic1();
 		case 'D':
 		case 'd':
-			//right
+			move(70, -70, 300);
 			handlingLogic1();
 		case 'X':
 		case 'x':
@@ -138,14 +145,12 @@ char* writeHandling() {
 			break;
 		}
 	}
-
 	moves[length] = '\0';
 	return moves;
 }
 
 int main(void) {
 	printf("Press 0 / A to record a new sequence, or 1 / B to play back an existing one: ");
-
 	int choice = -1;
 	while (true) {
 		if (stdinHasInput()) {
@@ -161,7 +166,6 @@ int main(void) {
 			break;
 		}
 	}
-
 	if (choice == '0') {
 		char* recorded = writeHandling();
 		if (recorded != NULL) {
@@ -176,6 +180,5 @@ int main(void) {
 	else {
 		printf("Invalid input. Please press 0 or 1.\n");
 	}
-
 	return 0;
 }
